345fcb31f361eebc368803eec3f96df549c30eaf
Alphamplyer <even@pouyadou.fr>
2020-10-06 18:06:04 +0200

    end chapter 23

--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -6,6 +6,10 @@ export default class RepLogCreator extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            quantityInputError: ''
+        }
+
         this.quantityInput = React.createRef();
         this.itemSelect = React.createRef();
 
@@ -27,6 +31,15 @@ export default class RepLogCreator extends Component {
         const itemSelect = this.itemSelect.current;
         const quantityInput = this.quantityInput.current;
 
+        if (quantityInput.value <= 0) {
+            this.setState({
+                quantityInputError: 'Please enter a value greater than 0'
+            })
+
+            // don't submit, or clear the form
+            return;
+        }
+
         onAddRepLog(
             itemSelect.options[itemSelect.selectedIndex].text,
             quantityInput.value
@@ -34,11 +47,16 @@ export default class RepLogCreator extends Component {
 
         itemSelect.selectedIndex = 0;
         quantityInput.value = '';
+        this.setState({
+            quantityInputError: ''
+        })
     }
 
     render() {
+        const { quantityInputError } = this.state;
+
         return (
-            <form className="form-inline" onSubmit={ this.handleFormSubmit } >
+            <form onSubmit={ this.handleFormSubmit } >
                 <div className="form-group">
                     <label className="sr-only control-label required" htmlFor="rep_log_item">
                         What did you lift?
@@ -55,7 +73,7 @@ export default class RepLogCreator extends Component {
                     </select>
                 </div>
                 {' '}
-                <div className="form-group">
+                <div className={`form-group ${ quantityInputError ? 'has-error' : '' }`}>
                     <label className="sr-only control-label required" htmlFor="rep_log_reps">
                         How many times?
                     </label>
@@ -63,7 +81,9 @@ export default class RepLogCreator extends Component {
                            ref={ this.quantityInput }
                            name="reps" required="required"
                            placeholder="How many times?"
-                           className="form-control"/>
+                           className="form-control"
+                    />
+                    { quantityInputError && <span className="help-block">{ quantityInputError }</span> }
                 </div>
                 {' '}
                 <button type="submit" className="btn btn-primary">I Lifted it!</button>
