a4d64374cfd502bab8cb0bcf8e8acc9c3f040394
Alphamplyer <even@pouyadou.fr>
2020-10-07 14:35:31 +0200

    end chapter 41

---
+++ assets/js/Components/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Button (props) {
+    const { className, ...otherProps } = props;
+
+    return (
+        <button
+            className={`btn ${ props.className }`}
+            {...otherProps}
+        >
+            {props.children}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.any,
+}
+
+Button.defaultProps = {
+    className: ''
+}
